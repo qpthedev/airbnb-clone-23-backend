5315b544e5be888837b178a4e38f236b38e9fa61
qpthedev <qpthedev@gmail.com>
2023-04-07 03:23:50 -0400

    Rooms admin

--- rooms/models.py
+++ rooms/models.py
@@ -11,6 +11,10 @@ class Room(CommonModel):
         PRIVATE_ROOM = "private_room", "Private Room"
         SHARED_ROOM = "shared_room", "Shared Room"
 
+    name = models.CharField(
+        max_length=180,
+        default="",
+    )
     country = models.CharField(
         max_length=50,
         default="USA",
@@ -41,6 +45,9 @@ class Room(CommonModel):
         "rooms.Amenity",
     )
 
+    def __str__(self):
+        return self.name
+
 
 class Amenity(CommonModel):
     # Amenity Definition
@@ -50,4 +57,11 @@ class Amenity(CommonModel):
     description = models.CharField(
         max_length=150,
         null=True,
+        blank=True,
     )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
