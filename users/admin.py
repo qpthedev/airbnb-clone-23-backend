ae8e81cdbaddd45e464da5a43148ce289fc0443f
qpthedev <qpthedev@gmail.com>
2023-04-06 23:52:52 -0400

    Add Users app. Delete Houses app.

---
+++ users/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+
+# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (
+            "Profile",
+            {
+                "fields": (
+                    "username",
+                    "name",
+                    "email",
+                    "password",
+                    "is_host",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Importand Dates",
+            {
+                "fields": (
+                    "date_joined",
+                    "last_login",
+                )
+            },
+        ),
+    )
+    list_display = (
+        "username",
+        "email",
+        "name",
+        "is_host",
+    )
