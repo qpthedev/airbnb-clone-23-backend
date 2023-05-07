87138b4649a56e854b4a4bbf1ecbf600c1281398
qpthedev <qpthedev@gmail.com>
2023-05-07 17:00:23 -0400

    custom admin search practice

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,14 +2,24 @@ from django.contrib import admin
 from .models import Room, Amenity
 
 
+@admin.action(description="Increase price by 5")
+def increase_price_5(model_admin, request, rooms):
+    for room in rooms.all():
+        room.price += 5
+        room.save()
+
+
 # Register your models here.
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
+    actions = (increase_price_5,)
+
     list_display = (
         "name",
         "price",
         "kind",
         "owner",
+        "rating",
         "created_at",
         "updated_at",
     )
@@ -23,6 +33,11 @@ class RoomAdmin(admin.ModelAdmin):
     readonly_fields = (
         "created_at",
         "updated_at",
+    )
+
+    search_fields = (
+        "name",
+        "price",
     )
 
 
