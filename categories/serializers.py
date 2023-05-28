787cd27c24d9aa20e70cbf7f763b919de45685a3
qpthedev <qpthedev@gmail.com>
2023-05-27 23:48:19 -0400

    POST request validation with is_valid

--- categories/serializers.py
+++ categories/serializers.py
@@ -2,5 +2,12 @@ from rest_framework import serializers
 
 
 class CategorySerializer(serializers.Serializer):
-    name = serializers.CharField(required=True)
-    kind = serializers.CharField()
+    name = serializers.CharField(
+        required=True,
+        max_length=50,
+    )
+    kind = serializers.CharField(
+        max_length=15,
+    )
+    pk = serializers.IntegerField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
