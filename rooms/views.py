2a1a071b9ac57ae3dc7d5066a964f00f4aeeb5e6
qpthedev <qpthedev@gmail.com>
2023-06-23 02:17:16 -0400

    room reviews and amenities pagination

--- rooms/views.py
+++ rooms/views.py
@@ -11,6 +11,7 @@ from rest_framework.status import HTTP_204_NO_CONTENT
 from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 from .models import Amenity, Room
 from categories.models import Category
+from reviews.serializers import ReviewSerializer
 
 
 class Rooms(APIView):
@@ -183,3 +184,55 @@ class AmenityDetail(APIView):
         amenity = self.get_object(pk)
         amenity.delete()
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+class RoomReviews(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        try:
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+
+        page_size = 3
+        start = (page - 1) * page_size
+        end = start + page_size
+
+        room = self.get_object(pk)
+        serializer = ReviewSerializer(
+            room.reviews.all()[start:end],
+            many=True,
+        )
+        return Response(serializer.data)
+
+
+class RoomAmenities(APIView):
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        try:
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+
+        page_size = 3
+        start = (page - 1) * page_size
+        end = start + page_size
+
+        room = self.get_object(pk)
+        serializer = AmenitySerializer(
+            room.amenities.all()[start:end],
+            many=True,
+        )
+        return Response(serializer.data)
